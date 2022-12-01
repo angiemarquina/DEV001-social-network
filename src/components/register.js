a1aa27354b44059e7609a57ba1ad978c132c1cb3
Addidaan <adriana48_rs@hotmail.com>
2022-12-01 10:51:29 -0600

    cambios css de register y agregue imagen de patita

--- src/components/register.js
+++ src/components/register.js
@@ -1,38 +1,43 @@
 export const register = (onNavigate) => {
   const registerDiv = document.createElement('div');
   registerDiv.className = 'registerDiv';
-  const logoDiv = document.createElement('div');
-  logoDiv.className = 'logoDiv';
+  const logoDivViewThree = document.createElement('div');
+  logoDivViewThree.className = 'logoDiv';
   const dataRegisterDiv = document.createElement('div');
   dataRegisterDiv.className = 'dataRegisterDiv';
 
   const footprint = document.createElement('img');
-  footprint.src = './imagenes/patita.png';
+  footprint.src = './imagenes/patitarosa.png';
   footprint.alt = 'la huella de una patita';
   footprint.className = 'footprint';
-  const title = document.createElement('h1');
-  title.textContent = 'CoHabita';
-  title.className = 'title';
+  const titleViewThree = document.createElement('h1');
+  titleViewThree.textContent = 'CoHabita';
+  titleViewThree.className = 'title';
   const name = document.createElement('input');
   name.type = 'text';
   name.placeholder = 'Nombre';
+  name.className = 'name';
   const mail = document.createElement('input');
   mail.type = 'email';
   mail.placeholder = 'Correo';
+  mail.className = 'mail';
   const password = document.createElement('input');
   password.placeholder = 'Contraseña';
   password.type = 'password';
+  password.className = 'password';
   const buttonRegister = document.createElement('button');
   buttonRegister.textContent = 'Registar';
+  buttonRegister.className = 'buttonRegister';
   const buttonHome = document.createElement('button');
   buttonHome.textContent = 'Regresar al Home';
+  buttonHome.className = 'buttonHome';
   buttonHome.addEventListener('click', () => onNavigate('/'));
 
-  registerDiv.appendChild(logoDiv);
+  registerDiv.appendChild(logoDivViewThree);
   registerDiv.appendChild(dataRegisterDiv);
 
-  logoDiv.appendChild(title);
-  logoDiv.appendChild(footprint);
+  logoDivViewThree.appendChild(footprint);
+  logoDivViewThree.appendChild(titleViewThree);
 
   dataRegisterDiv.appendChild(name);
   dataRegisterDiv.appendChild(mail);
